9f12f6f95a8139e0959897fa256d631d8d1fb200
Andrea Filice <filiceandrea8@gmail.com>
2026-03-03 21:54:08 +0100

    chore: fixing bugs
    * fixing small bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
         applicationId = "com.andreafilice.lavorami"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
-        var buildNumber = "02032026"
+        var buildNumber = "03032026"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
         resValue("string", "appVersionFull", ("$versionName ($buildNumber)"))
