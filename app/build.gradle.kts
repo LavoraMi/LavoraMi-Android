0cd79f59e45a42034b740e3d1479b503819c141e
Andrea Filice <filiceandrea8@gmail.com>
2026-03-18 23:29:55 +0100

    fix: fixing Account bugs
    * fixed Account Layout bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,9 @@ android {
         applicationId = "com.andreafilice.lavorami"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
+        versionCode = 17
         versionName = "1.0.0"
-        var buildNumber = "18032026"
+        var buildNumber = "180320261"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
         resValue("string", "appVersionFull", ("$versionName ($buildNumber)"))
