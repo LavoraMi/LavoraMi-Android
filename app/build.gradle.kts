8dec8048ad1ebf62cf808b9e01a24769e8fd85a4
Andrea Filice <filiceandrea8@gmail.com>
2026-06-26 09:50:38 +0200

    chore: bumped build number
    * bumped build number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         targetSdk = 36
         versionCode = 43
         versionName = "1.1.12"
-        var buildNumber = "25062026"
+        var buildNumber = "26062026"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
         resValue("string", "appVersionFull", ("$versionName ($buildNumber)"))
