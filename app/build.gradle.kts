0eb8edef2cdc5d15e7a7727ef80b5cdfe279118d
Andrea Filice <filiceandrea8@gmail.com>
2026-07-06 11:00:05 +0200

    chore(release): bumped version
    * bumped version number & code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.andreafilice.lavorami"
         minSdk = 24
         targetSdk = 36
-        versionCode = 49
-        versionName = "1.1.13"
+        versionCode = 50
+        versionName = "1.1.14"
         var buildNumber = "06072026"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
