c178571bf357e080e92d1f7931df4c0329847148
Andrea Filice <filiceandrea8@gmail.com>
2026-07-12 10:32:13 +0200

    chore: started a new release cycle
    * started a new release cycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,9 @@ android {
         applicationId = "com.andreafilice.lavorami"
         minSdk = 24
         targetSdk = 36
-        versionCode = 51
-        versionName = "1.1.15"
-        var buildNumber = "11072026"
+        versionCode = 52
+        versionName = "1.1.16"
+        var buildNumber = "12072026"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
         resValue("string", "appVersionFull", ("$versionName ($buildNumber)"))
