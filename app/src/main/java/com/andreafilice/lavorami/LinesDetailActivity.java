98a019b27fc7d2b75d91ceaebbe790bb684d5115
Andrea Filice <filiceandrea8@gmail.com>
2026-07-25 14:19:35 +0200

    fix: fixing some bugs
    * fixed some bugs with paddings

--- app/src/main/java/com/andreafilice/lavorami/LinesDetailActivity.java
+++ app/src/main/java/com/andreafilice/lavorami/LinesDetailActivity.java
@@ -2609,6 +2609,7 @@ public class LinesDetailActivity extends AppCompatActivity {
                 baseColor = ContextCompat.getColor(this, R.color.M2);
                 lineaRegolareIcon.setImageResource(R.drawable.ic_checkmark_metro);
                 statusText.setText(getString(R.string.metroStatoRegolare));
+                statusText.setPaddingRelative(0, 6, 10, 6);
                 infoIconStatus.setVisibility(View.GONE);
                 break;
             case "Rallentata":
@@ -2645,6 +2646,7 @@ public class LinesDetailActivity extends AppCompatActivity {
                 baseColor = ContextCompat.getColor(this, R.color.S12);
                 lineaRegolareIcon.setImageResource(R.drawable.ic_dark);
                 statusText.setText(getString(R.string.metroStatoChiusa));
+                statusText.setPaddingRelative(0, 6, 10, 6);
                 infoIconStatus.setVisibility(View.GONE);
                 break;
         }
