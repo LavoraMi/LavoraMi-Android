0b17142809ad0924e601e7e738aa46175ac5ff97
Rebuale <rebua2008@gmail.com>
2026-07-30 07:59:22 +0200

    fix: fix small bug
    when we reopened the app, some times the search bar was still full with cached datas, giving inconsistent results (searchbar ull with all resoult visible) now it clears onCreate

--- app/src/main/java/com/andreafilice/lavorami/LinesActivity.java
+++ app/src/main/java/com/andreafilice/lavorami/LinesActivity.java
@@ -121,6 +121,8 @@ public class LinesActivity extends AppCompatActivity {
         });
 
         searchLines = findViewById(R.id.editSearch);
+        searchLines.setText("");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
