f31f92e28ceba861dec1728439782530d01e70d4
Andrea Filice <filiceandrea8@gmail.com>
2026-08-04 23:32:50 +0200

    chore: bumped packages
    * bumped MapBox SDK to 11.27.1

--- app/src/main/java/com/andreafilice/lavorami/LibrariesActivity.java
+++ app/src/main/java/com/andreafilice/lavorami/LibrariesActivity.java
@@ -209,7 +209,7 @@ public class LibrariesActivity extends AppCompatActivity {
                 ),
                 new LibraryModel(
                         "Mapbox Maps SDK for Android",
-                        "11.27.0",
+                        "11.27.1",
                         "Mapbox Terms of Service",
                         "Copyright (c) Mapbox",
                         "Licensed under the Mapbox Terms of Service. See https://www.mapbox.com/legal/tos for details."
