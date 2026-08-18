2541723cdd77c9b637349c34e85743caf3b00954
Rebuale <rebua2008@gmail.com>
2026-08-18 23:44:55 +0200

    feat: added z641 main line

--- app/src/main/java/com/andreafilice/lavorami/StationDB.java
+++ app/src/main/java/com/andreafilice/lavorami/StationDB.java
@@ -450,6 +450,7 @@ public class StationDB {
             allStations.addAll(BusStationsDB.getStationsZ625());
             allStations.addAll(BusStationsDB.getStationsZ627());
             allStations.addAll(BusStationsDB.getStationsZ636());
+            allStations.addAll(BusStationsDB.getStationsZ641());
 
             allStations.addAll(FilobusStationsDB.getStationsFilobus90());
             allStations.addAll(FilobusStationsDB.getStationsFilobus91());
