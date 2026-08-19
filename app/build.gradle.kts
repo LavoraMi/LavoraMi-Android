7f4e15900ff56247ff94a0a39510f2671c36f887
Rebuale <rebua2008@gmail.com>
2026-08-19 10:07:10 +0200

    chore: bump build number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         targetSdk = 36
         versionCode = 58
         versionName = "1.18.4"
-        var buildNumber = "18082026"
+        var buildNumber = "19082026"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
         resValue("string", "appVersionFull", ("$versionName ($buildNumber)"))
