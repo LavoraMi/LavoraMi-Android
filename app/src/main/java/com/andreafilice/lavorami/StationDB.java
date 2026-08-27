4a1f409e72853cb8200cf00cbf77ff39175b2bef
Rebuale <rebua2008@gmail.com>
2026-08-27 10:08:21 +0200

    feat: stareted adding z649 mao

--- app/src/main/java/com/andreafilice/lavorami/StationDB.java
+++ app/src/main/java/com/andreafilice/lavorami/StationDB.java
@@ -456,6 +456,7 @@ public class StationDB {
             allStations.addAll(BusStationsDB.getStationsZ644());
             allStations.addAll(BusStationsDB.getStationsZ646());
             allStations.addAll(BusStationsDB.getStationsZ647());
+            allStations.addAll(BusStationsDB.getStationsZ649());
 
             allStations.addAll(FilobusStationsDB.getStationsFilobus90());
             allStations.addAll(FilobusStationsDB.getStationsFilobus91());
