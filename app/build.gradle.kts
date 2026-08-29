a7b6b477b9d606e2272a562f3e9e1cbf06182e42
Andrea Filice <filiceandrea8@gmail.com>
2026-08-29 17:11:02 +0200

    chore: bumped versions
    * started a new release cycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.andreafilice.lavorami"
         minSdk = 24
         targetSdk = 36
-        versionCode = 60
-        versionName = "1.19.0"
+        versionCode = 61
+        versionName = "1.20.0"
         var buildNumber = "29082026"
 
         resValue("string", "app_version", versionName ?: "1.0.0")
