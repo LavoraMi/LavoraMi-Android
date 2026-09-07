2ce0d9f3d530415c8c68f7581792365b4b93420c
Andrea Filice <filiceandrea8@gmail.com>
2026-09-07 19:56:41 +0200

    chore: revert commit
    * CodeQL doesn't support Kotlin 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     configurations.all {
         resolutionStrategy.eachDependency {
             if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("2.4.20")
+                useVersion("2.4.10")
             }
         }
     }
