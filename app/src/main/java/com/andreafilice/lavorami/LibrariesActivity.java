a810426237a01fe6f3106fea0e4bf57e62d305b7
Andrea Filice <filiceandrea8@gmail.com>
2026-09-10 11:39:50 +0200

    chore: bumped packages
    * bumped Firebase BOM

--- app/src/main/java/com/andreafilice/lavorami/LibrariesActivity.java
+++ app/src/main/java/com/andreafilice/lavorami/LibrariesActivity.java
@@ -150,21 +150,21 @@ public class LibrariesActivity extends AppCompatActivity {
                 ),
                 new LibraryModel(
                         "Firebase BOM",
-                        "34.18.0",
+                        "34.19.0",
                         "Apache License 2.0",
                         "Copyright Google LLC",
                         APACHE_LICENSE_TEXT
                 ),
                 new LibraryModel(
                         "Firebase Cloud Messaging",
-                        "34.18.0",
+                        "34.19.0",
                         "Apache License 2.0",
                         "Copyright Google LLC",
                         APACHE_LICENSE_TEXT
                 ),
                 new LibraryModel(
                         "Firebase Analytics",
-                        "34.18.0",
+                        "34.19.0",
                         "Apache License 2.0",
                         "Copyright Google LLC",
                         APACHE_LICENSE_TEXT
