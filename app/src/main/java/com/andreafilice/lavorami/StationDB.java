cbc59739a31f328aeb77b96fb0ee0ab08e466bbf
Rebuale <rebua2008@gmail.com>
2026-09-15 12:54:16 +0200

    feat: started adding z553

--- app/src/main/java/com/andreafilice/lavorami/StationDB.java
+++ app/src/main/java/com/andreafilice/lavorami/StationDB.java
@@ -405,6 +405,7 @@ public class StationDB {
 
             allStations.addAll(STAVStationsDB.getStationsZ551());
             allStations.addAll(STAVStationsDB.getStationsZ552());
+            allStations.addAll(STAVStationsDB.getStationsZ553());
 
             allStations.addAll(FilobusStationsDB.getStationsFilobus90());
             allStations.addAll(FilobusStationsDB.getStationsFilobus91());
