e4ba427a87bc987edf1093aa59773e3623a90c18
Andrea Filice <filiceandrea8@gmail.com>
2026-09-17 16:11:31 +0200

    feat: added maps
    * added z559 line map

--- app/src/main/java/com/andreafilice/lavorami/StationDB.java
+++ app/src/main/java/com/andreafilice/lavorami/StationDB.java
@@ -408,6 +408,7 @@ public class StationDB {
             allStations.addAll(STAVStationsDB.getStationsZ553());
             allStations.addAll(STAVStationsDB.getStationsZ554());
             allStations.addAll(STAVStationsDB.getStationsZ557());
+            allStations.addAll(STAVStationsDB.getStationsZ559());
 
             allStations.addAll(FilobusStationsDB.getStationsFilobus90());
             allStations.addAll(FilobusStationsDB.getStationsFilobus91());
