1a8ee5b9854e8fb81e87cb2f34ac368cbd6e2615
Andrea Filice <filiceandrea8@gmail.com>
2026-09-17 21:26:54 +0200

    feat: finishing maps
    * finished z560 map line

--- app/src/main/java/com/andreafilice/lavorami/StationDB.java
+++ app/src/main/java/com/andreafilice/lavorami/StationDB.java
@@ -409,6 +409,7 @@ public class StationDB {
             allStations.addAll(STAVStationsDB.getStationsZ554());
             allStations.addAll(STAVStationsDB.getStationsZ557());
             allStations.addAll(STAVStationsDB.getStationsZ559());
+            allStations.addAll(STAVStationsDB.getStationsZ560());
 
             allStations.addAll(FilobusStationsDB.getStationsFilobus90());
             allStations.addAll(FilobusStationsDB.getStationsFilobus91());
