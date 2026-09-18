92c2c936dde0f7247f6b00cf99c350a0e4c7f84c
Rebuale <rebua2008@gmail.com>
2026-09-18 11:54:26 +0200

    feat: added main route z555

--- app/src/main/java/com/andreafilice/lavorami/LinesDetailActivity.java
+++ app/src/main/java/com/andreafilice/lavorami/LinesDetailActivity.java
@@ -1808,7 +1808,7 @@ public class LinesDetailActivity extends AppCompatActivity {
         String[] regionalDeviationsLinks = cdnData.getRegionalLinesDeviationsLinks();
         String[] linesSuspended = cdnData.getlineeSospeseInteramente();
         String[] lineeSostituiteBus = cdnData.getlineeSostituiteBus();
-        String[] linesMultipleRoutes = {"z555", "z619", "z647"};
+        String[] linesMultipleRoutes = {"z301", "z555", "z619", "z647"};
 
         LinearLayout deviazioneLinea = findViewById(R.id.deviazioneLinea);
         LinearLayout lineaSospesa = findViewById(R.id.lineaInterrotta);
