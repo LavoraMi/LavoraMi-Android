6324693eecf5ac2aed4b084e0324b2bea6ddb630
Tommaso Ruggeri <tommasoruggeri2008@gmail.com>
2026-09-18 15:25:30 +0200

    feat: added class for NET Bus Lines

---
+++ app/src/main/java/com/andreafilice/lavorami/stationsDB/NETStationsDB.java
@@ -0,0 +1,12 @@
+package com.andreafilice.lavorami.stationsDB;
+import com.andreafilice.lavorami.MetroStation;
+
+import java.util.ArrayList;
+import java.util.List;
+public class NETStationsDB {
+    public static List<MetroStation> getStationsZ301() {
+        List<MetroStation> stations = new ArrayList<>();
+
+        return stations;
+    }
+}
