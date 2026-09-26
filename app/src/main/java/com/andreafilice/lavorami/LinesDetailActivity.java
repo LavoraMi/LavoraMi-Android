0c71f2629046d8bcd972874e608166579789985d
Andrea Filice <filiceandrea8@gmail.com>
2026-09-26 09:01:41 +0200

    chore: preparing for release
    * preparing files to Release

--- app/src/main/java/com/andreafilice/lavorami/LinesDetailActivity.java
+++ app/src/main/java/com/andreafilice/lavorami/LinesDetailActivity.java
@@ -175,7 +175,7 @@ public class LinesDetailActivity extends AppCompatActivity {
         ArrayList<String> busLinesWithMap = new ArrayList<>(Arrays.asList("z551", "z552", "z553", "z554", "z555", "z556", "z557", "z559", "z560", "z601", "z602", "z603", "z606", "z611", "z612", "z616", "z617", "z618", "z619", "z620", "z621", "z622", "z625", "z627", "z636", "z641", "z642", "z643", "z644", "z646", "z647", "z649", "z6C3")); //STAV + Movibus
 
         ArrayList<String> busWithMapsNotInRelease = new ArrayList<>(Arrays.asList("z301","z304")); //NET
-        busLinesWithMap.addAll(busWithMapsNotInRelease);
+        //busLinesWithMap.addAll(busWithMapsNotInRelease);
 
         lavoriNested = findViewById(R.id.lavoriNested);
         interscambiNested = findViewById(R.id.interscambiNested);
